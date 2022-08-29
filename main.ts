2c0fbaf42b758c02bd269af4d419978ab5ee0fe9
Jdomasin <111815565+Jdomasin@users.noreply.github.com>
2022-08-29 12:50:01 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onGesture(Gesture.Shake, function () {
+    music.playMelody("C5 B A G F E D C ", 120)
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(1000)
+    basic.clearScreen()
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.clearScreen()
+})
